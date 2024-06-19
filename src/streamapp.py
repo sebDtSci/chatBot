16f82911de74c949e50b357ab941abd46b1ca047
Seb <121176289+sebDtSci@users.noreply.github.com>
2024-06-19 16:56:43 +0200

    add model choice

--- src/streamapp.py
+++ src/streamapp.py
@@ -2,11 +2,19 @@ import streamlit as st
 from generateS import Generate
 from saveConversation import save_conversation, load_conversations
 from memory import ChatbotMemory
+import subprocess
+
+def get_model_list():
+    result = subprocess.run(["ollama", "list"], capture_output=True, text=True)
+    lines = result.stdout.strip().split('\n')
+    models = [line.split()[0] for line in lines[1:]] 
+    return models
 
 def main():
     st.title("Chatbot Interface with Memory")
-    
-    model_options = ["aya:35b", "openchat:latest", "llama3:latest"]
+
+    model_options = get_model_list()
+    # model_options = ["aya:35b", "openchat:latest", "llama3:latest"]
     selected_model = st.selectbox("Choisissez le modèle" , model_options)
 
     if "chatbot" not in st.session_state or st.session_state.model_name != selected_model:
@@ -24,17 +32,13 @@ def main():
     conversation_titles = conversations_df["Titre"].tolist()
     selected_conversation = st.sidebar.selectbox("Sélectionnez une conversation", conversation_titles)
     if st.sidebar.button("Load") and selected_conversation:
-        # conversation_data = conversations_df[conversations_df["Titre"] == selected_conversation]["Conversation"].values[0]
-        chatbot.remember(conversations_df)
-        # st.sidebar.text_area("Conversation", conversation_data, height=400)
-    
+        chatbot.remember(conversations_df)    
 
     user_input = st.text_input("You:", key="input")
 
     if st.button("Send") and user_input:
         # Afficher le message de l'utilisateur immédiatement
         st.session_state.history.append({"user": user_input, "bot": ""})
-        # st.experimental_rerun()
         st.rerun()
 
     # Gestion des réponses du chatbot après l'envoi du message
@@ -47,50 +51,33 @@ def main():
         for chunk in response_generator:
             response += chunk
             response_placeholder.markdown(f"""
-            <div style="text-align: left; background-color: #229954 ; padding: 10px; border-radius: 10px; margin: 10px 0;">
-                <b>Stem:</b> {response}
+        <div style="text-align: left; padding: 10px; margin: 10px 0;">
+            <div><b>Stem:</b></div>
+            <div style="background-color: #229954; border-radius: 10px; padding: 10px;">
+                {response}
             </div>
-            """, unsafe_allow_html=True)
-            
-        #     response_placeholder.markdown(f"""
-        # <div style="text-align: left; padding: 10px; margin: 10px 0;">
-        #     <div><b>Stem:</b></div>
-        #     <div style="background-color: #229954; border-radius: 10px; padding: 10px;">
-        #         {response}
-        #     </div>
-        # </div>
-        # """, unsafe_allow_html=True)
+        </div>
+        """, unsafe_allow_html=True)
         
         st.session_state.history[-1]["bot"] = response
-        # st.experimental_rerun()
         st.rerun()
     
     # Affichage de l'historique des conversations
     for chat in reversed(st.session_state.history):
-    #     user_message = f"""
-    #     <div style="text-align: right; padding: 10px; margin: 10px 0;">
-    #         <div><b>Vous:</b></div>
-    #         <div style="background-color: #2471A3; border-radius: 10px; padding: 10px;">
-    #             {chat['user']}
-    #         </div>
-    #     </div>
-    #     """
-    #     bot_message = f"""
-    #     <div style="text-align: left; padding: 10px; margin: 10px 0;">
-    #         <div><b>Stem:</b></div>
-    #         <div style="background-color: #229954; border-radius: 10px; padding: 10px;">
-    #             {chat['bot']}
-    #         </div>
-    #     </div>
-    #     """
         user_message = f"""
-        <div style="text-align: right; background-color: #2471A3; padding: 10px; border-radius: 10px; margin: 10px 0;">
-            <b>Vous:</b> {chat['user']}
+        <div style="text-align: right; padding: 10px; margin: 10px 0;">
+            <div><b>Vous:</b></div>
+            <div style="background-color: #2471A3; border-radius: 10px; padding: 10px;">
+                {chat['user']}
+            </div>
         </div>
         """
         bot_message = f"""
-        <div style="text-align: left; background-color: #229954 ; padding: 10px; border-radius: 10px; margin: 10px 0;">
-            <b>Stem:</b> {chat['bot']}
+        <div style="text-align: left; padding: 10px; margin: 10px 0;">
+            <div><b>Stem:</b></div>
+            <div style="background-color: #229954; border-radius: 10px; padding: 10px;">
+                {chat['bot']}
+            </div>
         </div>
         """
         st.markdown(bot_message, unsafe_allow_html=True)
@@ -103,128 +90,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-###################################################################################################################################################################
-# import streamlit as st
-# from generateS import Generate
-# import logging
-
-# def main():
-#     st.title("Chatbot Interface with Memory")
-    
-#     # model_name = "aya:35b"
-#     model_options = ["aya:35b", "openchat:latest", "llama3:latest"]
-#     selected_model = st.selectbox("Choisissez le modèle", model_options)
-
-#     # if "chatbot" not in st.session_state:
-#     #     st.session_state.chatbot = Generate(model=selected_model)
-#     if "chatbot" not in st.session_state or st.session_state.model_name != selected_model:
-#         st.session_state.chatbot = Generate(model=selected_model)
-#         st.session_state.model_name = selected_model
-    
-#     chatbot = st.session_state.chatbot
-
-#     if "history" not in st.session_state:
-#         st.session_state.history = []
-
-#     user_input = st.text_input("You:", key="input")
-    
-
-#     if st.button("Send"):
-#         if user_input:
-#             response_generator = chatbot.ans(user_input)
-#             response = ""
-#             response_placeholder = st.empty()
-            
-#             # écrit le message de l'utilisateur dans le streamlit
-#         elif st.session_state.history == []:
-#             user_message = f"""
-#             <div style="text-align: right; background-color: #GREEN; padding: 10px; border-radius: 10px; margin: 10px 0;">
-#                 <b>Vous:</b> {user_input}
-#             </div>
-#             """
-#             st.markdown(user_message, unsafe_allow_html=True)
-#         else:
-#             for chat in reversed(st.session_state.history):
-#                 user_message = f"""
-#                 <div style="text-align: right; background-color: #GREEN; padding: 10px; border-radius: 10px; margin: 10px 0;">
-#                     <b>Vous:</b> {chat['user']}
-#                 </div>
-#                 """
-#                 # bot_message = f"""
-#                 # <div style="text-align: left; background-color: #BLUE; padding: 10px; border-radius: 10px; margin: 10px 0;">
-#                 #     <b>Stem:</b> {chat['bot']}
-#                 # </div>
-#                 # """
-#                 # st.markdown(bot_message, unsafe_allow_html=True)
-#                 st.markdown(user_message, unsafe_allow_html=True)
-                
-
-#             for chunk in response_generator:
-#                 response += chunk
-#                 response_placeholder.markdown(f"""
-#                 <div style="text-align: left; background-color: #BLUE; padding: 10px; border-radius: 10px; margin: 10px 0;">
-#                     <b>Stem:</b> {response}
-#                 </div>
-#                 """, unsafe_allow_html=True)
-            
-#             st.session_state.history.append({"user": user_input, "bot": response})
-
-#     # for chat in reversed(st.session_state.history):
-#     #     user_message = f"""
-#     #     <div style="text-align: right; background-color: #GREEN; padding: 10px; border-radius: 10px; margin: 10px 0;">
-#     #         <b>Vous:</b> {chat['user']}
-#     #     </div>
-#     #     """
-#     #     bot_message = f"""
-#     #     <div style="text-align: left; background-color: #BLUE; padding: 10px; border-radius: 10px; margin: 10px 0;">
-#     #         <b>Stem:</b> {chat['bot']}
-#     #     </div>
-#     #     """
-#     #     st.markdown(bot_message, unsafe_allow_html=True)
-#     #     st.markdown(user_message, unsafe_allow_html=True)
-        
-
-# if __name__ == "__main__":
-#     main()
-
-## Stream False:
-
-# def main():
-#     st.title("Chatbot Interface with Memory")
-    
-#     model_name = "aya:35b" 
-
-#     # Initialiser le chatbot dans st.session_state si ce n'est pas déjà fait
-#     if "chatbot" not in st.session_state:
-#         st.session_state.chatbot = Generate(model=model_name)
-    
-#     chatbot = st.session_state.chatbot
-
-#     if "history" not in st.session_state:
-#         st.session_state.history = []
-
-#     user_input = st.text_input("You:", key="input")
-
-#     if st.button("Send"):
-#         if user_input:
-#             response = chatbot.ans(user_input)
-#             st.session_state.history.append({"user": user_input, "bot": response})
-
-#     # Affichage de l'historique des conversations en ordre inversé
-#     for chat in reversed(st.session_state.history):
-#         user_message = f"""
-#         <div style="text-align: right; background-color: #GREEN; padding: 10px; border-radius: 10px; margin: 10px 0;">
-#             <b>Vous:</b> {chat['user']}
-#         </div>
-#         """
-#         bot_message = f"""
-#         <div style="text-align: left; background-color: #BLUE; padding: 10px; border-radius: 10px; margin: 10px 0;">
-#             <b>Stem:</b> {chat['bot']}
-#         </div>
-#         """
-#         st.markdown(bot_message, unsafe_allow_html=True)
-#         st.markdown(user_message, unsafe_allow_html=True)
-        
-
-# if __name__ == "__main__":
-#     main()
