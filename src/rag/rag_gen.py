5a6580f42f4751c7c291c26823118469b4adf615
Seb <121176289+sebDtSci@users.noreply.github.com>
2024-06-20 18:31:33 +0200

    add some modification at the rag folder

--- src/rag/rag_gen.py
+++ src/rag/rag_gen.py
@@ -1,7 +1,10 @@
 import chromadb
 from chromadb.config import Settings
 from src.rag.document_reader import read_docx
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 Settings = Settings()
 client = chromadb.Client(Settings)
 
+# TODO: toujours utiliser le même tokenisateur
+name_model = 'openchat:latest'
